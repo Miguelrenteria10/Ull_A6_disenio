0932a285e929d3e9e22398b72490cc968f61411d
MIGUEL_JARED_RENTERIA_MARRUFO <a.21308051281099@cbtis128.edu.mx>
2024-04-26 15:04:19 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:uii_a6_disenio/screens/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
