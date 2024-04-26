98506b570070528d0ef2934991464a51ee802f61
MIGUEL_JARED_RENTERIA_MARRUFO <a.21308051281099@cbtis128.edu.mx>
2024-04-26 15:24:54 +0000

    Actualizado

--- lib/data/product_data.dart
+++ lib/data/product_data.dart
@@ -2,15 +2,15 @@ import 'package:uii_a6_disenio/modelos/product_model.dart';
 
 List<ProductModel> products = [
   ProductModel(
-      img: "assets/images/jordan.png",
-      title: "Jordan Tradicional",
-      description: "Mas vendidos"),
+      img: "assets/images/coca1.png",
+      title: "Coca Cola De Vidrio",
+      description: "Mas vendida"),
   ProductModel(
-      img: "assets/images/jordan1.png",
-      title: "Jordan Blue&White",
+      img: "assets/images/coca2.png",
+      title: "Cocacola De 2.5L",
       description: "Los especiales de la casa"),
   ProductModel(
-      img: "assets/images/jordan2.png",
-      title: "Air Jordan",
-      description: "Nuevos, pero especiales"),
+      img: "assets/images/image3.png",
+      title: "Cocacola De 355ml",
+      description: "Muy buena para el antojo"),
 ];
